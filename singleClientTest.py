9522b9c82f7f16fb21f28fc4ff61b44a9c2c108e
Lemmore <Leuchteintraum@gmail.com>
2019-05-12 17:45:32 -0700

    Update singleClientTest.py

--- singleClientTest.py
+++ singleClientTest.py
@@ -2,6 +2,7 @@ import time
 import threading
 import MalmoPython
 import random
+from collections import defaultdict
 
 def drawMobs(num_mobs):
     xml = ""
@@ -21,8 +22,8 @@ def drawItems(num_items):
     return xml
 
 
-def getXML(num_mobs = 0, num_items = 4, agent_names = ['A']):
-    mission_name = 'multi_agent'
+def getXML(num_mobs = 0, num_items = 4, agent_name="iKun"):
+    mission_name = 'iKun'
 
     mission_xml = '''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
         <Mission xmlns="http://ProjectMalmo.microsoft.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
@@ -44,121 +45,120 @@ def getXML(num_mobs = 0, num_items = 4, agent_names = ['A']):
                 <DrawCuboid x1="-20" y1="200" z1="-20" x2="20" y2="200" z2="20" type="glowstone"/>
                 <DrawCuboid x1="-19" y1="200" z1="-19" x2="19" y2="227" z2="19" type="stained_glass" colour="RED"/>
                 <DrawCuboid x1="-18" y1="202" z1="-18" x2="18" y2="247" z2="18" type="air"/>
-                <DrawBlock x="0" y="226" z="0" type="fence"/>''' + drawMobs(10) + drawItems(10) + '''
+                <DrawBlock x="0" y="226" z="0" type="fence"/>''' + drawMobs(num_mobs) + drawItems(num_items) + '''
               </DrawingDecorator>
               <ServerQuitFromTimeUp description="" timeLimitMs="50000"/>
             </ServerHandlers>
           </ServerSection>'''
 
-    # Add an agent section for each robot. Robots run in survival mode.
-    # Give each one a wooden pickaxe for protection...
-    for i in range(len(agent_names[:-1])):
-        mission_xml += '''<AgentSection mode="Survival">
-            <Name>''' + agent_names[i] + '''</Name>
-            <AgentStart>
-              <Placement x="''' + str(random.randint(-17, 17)) + '''" y="204" z="''' + str(
-            random.randint(-17, 17)) + '''"/>
-              <Inventory>
-                <InventoryObject type="iron_axe" slot="0" quantity="1"/>
-                <InventoryItem type="planks" variant="acacia"/>
-              </Inventory>
-            </AgentStart>
-            <AgentHandlers>
-              <ContinuousMovementCommands turnSpeedDegs="180"/>
-              <RewardForCollectingItem>
-                <Item type="apple" reward="10"/>
-              </RewardForCollectingItem>
-              <RewardForTimeTaken initialReward="0" delta="1" density="PER_TICK"/>
-              <RewardForDamagingEntity>
-                <Mob type="Zombie" reward="1"/>
-              </RewardForDamagingEntity>
-              <RewardForSendingCommand reward="-1"/>
-              <VideoProducer>
-                <Width>512</Width>
-                <Height>512</Height>
-              </VideoProducer>
-              <ObservationFromFullStats/>
-            </AgentHandlers>
-          </AgentSection>'''
-
-    # Add a section for the observer. Observer runs in creative mode.
-    mission_xml += '''<AgentSection mode="Creative">
-        <Name>''' + agent_names[-1] + '''</Name>
-        <AgentStart>
-          <Placement x="0.5" y="228" z="0.5" pitch="90"/>
-        </AgentStart>
-        <AgentHandlers>
-          <VideoProducer>
-            <Width>512</Width>
-            <Height>512</Height>
-          </VideoProducer>
-          <ObservationFromFullStats/>
-        </AgentHandlers>
-      </AgentSection>'''
+    # Agent section
+    mission_xml += '''<AgentSection mode="Survival">
+                <Name>''' + agent_name + '''</Name>
+                <AgentStart>
+                  <Placement x="''' + str(random.randint(-17, 17)) + '''" y="204" z="''' + str(
+        random.randint(-17, 17)) + '''"/>
+                <Inventory>
+                  <InventoryObject type="iron_axe" slot="0" quantity="1"/>
+                </Inventory>
+                </AgentStart>
+                <AgentHandlers>
+                  <ContinuousMovementCommands turnSpeedDegs="180"/>
+                  <RewardForCollectingItem>
+                    <Item type="apple" reward="10"/>
+                  </RewardForCollectingItem>
+                  <RewardForTimeTaken initialReward="0" delta="1" density="PER_TICK"/>
+                  <RewardForDamagingEntity>
+                    <Mob type="Zombie" reward="1"/>
+                  </RewardForDamagingEntity>
+                  <RewardForSendingCommand reward="-1"/>
+                  <VideoProducer>
+                    <Width>512</Width>
+                    <Height>512</Height>
+                  </VideoProducer>
+                  <ObservationFromFullStats/>
+                </AgentHandlers>
+              </AgentSection>'''
 
     mission_xml += '''</Mission>'''
     return mission_xml
 
-class ThreadedAgent(threading.Thread):
-    def __init__(self, role, clientPool, missionXML):
-        threading.Thread.__init__(self)
-        print("Initialize thread %s" % role)
-        self.role = role
-        self.client_pool = clientPool
-        self.mission_xml = missionXML
-        self.agent_host = MalmoPython.AgentHost()
-        self.agent_host.setDebugOutput(False)
-        self.mission = MalmoPython.MissionSpec(missionXML, True)
-        self.mission_record = MalmoPython.MissionRecordSpec()
-        self.actions = ['turn -0.5', 'move 1', 'jump 1']
-        self.reward = 0
-        self.mission_end_message = ""
-        print("Finish initialization.")
 
-    def run(self):
-        max_retries = 3
-        for retry in range(max_retries):
-            try:
-                # Attempt to start the mission:
-                self.agent_host.startMission(self.mission, self.client_pool, self.mission_record, self.role, "WTF")
-                print("Mission started.")
-                break
-            except RuntimeError as e:
-                if retry == max_retries - 1:
-                    print("Error starting mission",e)
-                    print("Is the game running?")
-                    exit(1)
-                else:
-                    time.sleep(1)
+class iKun(object):
 
-        world_state = self.agent_host.getWorldState()
-        while not world_state.has_mission_begun:
-            time.sleep(0.1)
-            world_state = self.agent_host.getWorldState()
+    def __init__(self):
+        self.speed = 2
+        self.turning = 1
+        self.ratio = 1.0
+        self.defense = 1.0
+        self.dodge = 0.0
 
-        self.runMissionLoop()
+        self.is_attacking = False
+        self.is_crouching = False
+        self.is_turning = False
+        self.is_jumping = False
 
-    def runMissionLoop(self):
-        turn_key = ""
-        times = 100
+        self.inventory = defaultdict(lambda: 0, {})
+        self.actions = ['move', 'turn +', 'turn -', 'jump', 'attack', 'crouch']
+
+    def run(self, agent_host):
         while True:
-            times -= 1
-            if times < 0: break
-            world_state = self.agent_host.getWorldState()
-            if not world_state.is_mission_running:
-                break
+            action = random.choice(self.actions)
+            command = ''
 
-            self.agent_host.sendCommand(random.choice(self.actions))
+            if action == 'move':
+                current_speed = self.ratio * self.speed
+                command = action + ' ' + str(current_speed)
 
-        # Close our agent hosts:
-        time.sleep(2)
-        self.agent_host = None
+            elif 'turn' in action:
+                if self.is_turning:
+                    command = 'turn 0'
+                else:
+                    current_turning = self.ratio * self.turning
+                    command = action + str(current_turning)
+
+            elif action == 'jump':
+                if self.is_jumping:
+                    command = 'jump 0'
+                else:
+                    command = 'jump 1'
+
+            elif action == 'attack':
+                if self.is_attacking:
+                    command = 'attack 0'
+                else:
+                    command = 'attack 1'
+
+            elif action == 'crouch':
+                if self.is_crouching:
+                    command = 'crouch 0'
+                else:
+                    command = 'crouch 1'
+
+            print("Command:", command)
+            agent_host.sendCommand(command)
+            time.sleep(0.1)
+
 
 if __name__ == '__main__':
     my_client_pool = MalmoPython.ClientPool()
     my_client_pool.add(MalmoPython.ClientInfo("127.0.0.1", 10000))
     mission_xml = getXML()
-    agent = ThreadedAgent(0, my_client_pool, mission_xml)
-    agent.start()
-    agent.join()
+    agent_host = MalmoPython.AgentHost()
+    agent_host.setDebugOutput(False)
+
+    ikun = iKun()
+
+    num_reps = 1
+    for iRepeat in range(num_reps):
+        mission = MalmoPython.MissionSpec(mission_xml, True)
+        mission_record = MalmoPython.MissionRecordSpec()
+        agent_host.startMission(mission, my_client_pool, mission_record, 0, "iKun")
+
+        world_state = agent_host.getWorldState()
+        while not world_state.has_mission_begun:
+            time.sleep(0.1)
+            world_state = agent_host.getWorldState()
+
+            ikun.run(agent_host)
+
     time.sleep(10000)
