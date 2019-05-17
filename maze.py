b20850d0f5ce5f858918b2196114d2d50bf17469
Lemmore <Leuchteintraum@gmail.com>
2019-05-17 15:11:09 -0700

    Update maze.py

--- maze.py
+++ maze.py
@@ -7,7 +7,7 @@ from keras.optimizers import SGD , Adam, RMSprop
 from keras.layers.advanced_activations import PReLU
 import MalmoPython
 
-ACT_DICT = {'n': 'movenorth 1', 's': 'movesouth 1', 'w': 'movewest 1', 'e': 'moveeast 1'}
+ACTIONS = ['movenorth 1', 'movesouth 1', 'movewest 1', 'moveeast 1']
 
 MAP_LENGTH = 20
 MAP_WIDTH = 20
@@ -16,6 +16,7 @@ UNKNOWN = 0
 AIR = 1
 LAND = 2
 TARGET = 3
+SELF = 4
 
 epsilon = 0.1
 gamma = 0.5
@@ -63,6 +64,10 @@ def GetMissionXML():
                 </AgentStart>
                 <AgentHandlers>
                     <DiscreteMovementCommands/>
+                    <RewardForSendingCommand reward="-1"/>
+                    <RewardForTouchingBlockType>
+                      <Block reward="100" type="redstone_block" behaviour="onceOnly"/>
+                    </RewardForTouchingBlockType>
                     <AgentQuitFromTouchingBlockType>
                         <Block type="redstone_block"/>
                     </AgentQuitFromTouchingBlockType>
@@ -77,14 +82,14 @@ def GetMissionXML():
             </Mission>'''
 
 
-def build_model(maze, lr=0.001):
-    global ACT_DICT
+def build_model(lr=0.001):
+    global ACTIONS
     model = Sequential()
-    model.add(Dense(maze.size, input_shape=(maze.size,)))
+    model.add(Dense((MAP_LENGTH, MAP_WIDTH), input_shape=((MAP_LENGTH, MAP_WIDTH),)))
     model.add(PReLU())
-    model.add(Dense(maze.size))
+    model.add(Dense((MAP_LENGTH, MAP_WIDTH)))
     model.add(PReLU())
-    model.add(Dense(len(ACT_DICT)))
+    model.add(Dense(len(ACTIONS)))
     model.compile(optimizer='adam', loss='mse')
     return model
 
@@ -103,24 +108,24 @@ class iKun(object):
         if len(self.memory) > self.memory_length:
             del self.memory[0]
 
-    def predict(self, status):
-        return self.model.predict(status)[0]
+    def predict(self, canvas):
+        return self.model.predict(canvas)[0]
 
     def train(self, batch_size=5):
-        global ACT_DICT
+        global ACTIONS
 
         env_size = self.memory[0][0].shape[1]
         mem_size = len(self.memory)
         data_size = min(mem_size, batch_size)
 
         inputs = np.zeros((data_size, env_size))
-        targets = np.zeros((data_size, len(ACT_DICT)))
+        targets = np.zeros((data_size, len(ACTIONS)))
 
         for i, j in enumerate(np.random.choice(range(mem_size), data_size, replace = False)):
-            envstate, action, reward, envstate_next, game_over = self.memory[j]
-            inputs[i] = envstate
-            targets[i] = self.predict(envstate)
-            Q_sa = np.max(self.predict(envstate_next))
+            canvas, action, reward, canvas_next, game_over = self.memory[j]
+            inputs[i] = canvas
+            targets[i] = self.predict(canvas)
+            Q_sa = np.max(self.predict(canvas_next))
             if game_over:
                 targets[i][action] = reward
             else:
@@ -132,10 +137,12 @@ class iKun(object):
 
 class Maze(object):
 
-    def __init__(self, agent_host):
+    def __init__(self, agent_host, agent):
         self.agent_host = agent_host
         self.maze = np.zeros((MAP_LENGTH, MAP_WIDTH))
         self.position, self.target = self.get_position_target()
+        self.agent = agent
+        self.reward = 0
 
     def get_position_target(self):
         global MAP_LENGTH, MAP_WIDTH
@@ -152,10 +159,56 @@ class Maze(object):
         for i in range(len(grid)):
             if grid[i] == "redstone_block":
                 target = [i // MAP_WIDTH, i % MAP_WIDTH]
-            elif grid[i] == "diamond_block":
+            elif grid[i] == "emerald_block":
                 position = [i // MAP_WIDTH, i % MAP_WIDTH]
 
         return position, target
+
+    def get_canvas(self):
+        global SELF, TARGET
+        canvas = np.copy(self.maze)
+        canvas[self.position[0]][self.position[1]] = SELF
+        canvas[self.target[0]][self.target[1]] = TARGET
+        return canvas
+
+    def run(self):
+        global ACTIONS, LAND
+        canvas = self.get_canvas()
+
+        while True:
+            prev_canvas = canvas
+            reward = 0
+            rnd = random.random()
+
+            if rnd < self.agent.epsilon:
+                action = random.randint(0, 3)
+            else:
+                action = np.argmax(self.agent.predict(canvas))
+
+            agent_host.sendCommand(ACTIONS[action])
+            world_state = self.agent_host.getWorldState()
+            game_over = False if world_state.is_mission_running else True
+
+            # Update recognized maze
+            if world_state.number_of_observations_since_last_state > 0:
+                msg = world_state.observations[-1].text
+                ob = json.loads(msg)
+                print(ob)
+                if "Reward" in ob:
+                    current_reward = ob["Reward"]
+                    if current_reward > self.reward:  # 100 for reaching target
+                        self.maze[self.position[0]][self.position[1]] = TARGET
+                    elif current_reward + 1 == self.reward:  # -1 for each step
+                        self.maze[self.position[0]][self.position[1]] = LAND
+                    elif game_over:  # -20 for falling
+                        self.maze[self.position[0]][self.position[1]] = AIR
+                        current_reward = self.reward - 20
+                    reward = current_reward - self.reward
+                    self.reward = current_reward
+
+            status = [prev_canvas, action, reward, canvas, game_over]
+            self.agent.memorize(status)
+            self.agent.train()
 
 
 if __name__ == '__main__':
@@ -165,9 +218,10 @@ if __name__ == '__main__':
     agent_host = MalmoPython.AgentHost()
     agent_host.setDebugOutput(False)
 
-    ikun = iKun()
+    model = build_model()
+    ikun = iKun(model)
 
-    num_reps = 1
+    num_reps = 10
     for iRepeat in range(num_reps):
         mission = MalmoPython.MissionSpec(mission_xml, True)
         mission_record = MalmoPython.MissionRecordSpec()
@@ -176,6 +230,7 @@ if __name__ == '__main__':
         world_state = agent_host.getWorldState()
         while not world_state.has_mission_begun:
             time.sleep(0.1)
-            ikun.run(agent_host)
+            maze = Maze(agent_host, ikun)
+            maze.run()
 
     time.sleep(10000)
