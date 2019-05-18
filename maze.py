b1ec20177fae60faf8664077d7d142a98e711371
Lemmore <Leuchteintraum@gmail.com>
2019-05-18 14:28:51 -0700

    Finish Debug

--- maze.py
+++ maze.py
@@ -9,17 +9,15 @@ import MalmoPython
 
 ACTIONS = ['movenorth 1', 'movesouth 1', 'movewest 1', 'moveeast 1']
 
-MAP_LENGTH = 20
-MAP_WIDTH = 20
+SIZE = 10
+MAP_LENGTH = 2 * SIZE + 1
+MAP_WIDTH = 2 * SIZE + 1
 
 UNKNOWN = 0
 AIR = 1
 LAND = 2
 TARGET = 3
 SELF = 4
-
-epsilon = 0.1
-gamma = 0.5
 
 
 def GetMissionXML():
@@ -44,12 +42,14 @@ def GetMissionXML():
                     <DrawSphere x="-27" y="70" z="0" radius="30" type="air"/>
                   </DrawingDecorator>
                   <MazeDecorator>
-                    <SizeAndPosition width="''' + str(MAP_WIDTH) + '''" length="''' + str(MAP_LENGTH) + '''" height="10" xOrigin="-32" yOrigin="69" zOrigin="-5"/>
+                    <Seed>0</Seed>
+                    <SizeAndPosition width="''' + str(SIZE) + '''" length="''' + str(SIZE) + '''" height="10" xOrigin="-32" yOrigin="69" zOrigin="-5"/>
                     <StartBlock type="emerald_block" fixedToEdge="true"/>
                     <EndBlock type="redstone_block" fixedToEdge="true"/>
                     <PathBlock type="diamond_block"/>
                     <FloorBlock type="air"/>
                     <GapBlock type="air"/>
+                    <GapProbability>''' + str(0.5) + '''</GapProbability>
                     <AllowDiagonalMovement>false</AllowDiagonalMovement>
                   </MazeDecorator>
                   <ServerQuitFromTimeUp timeLimitMs="10000"/>
@@ -70,6 +70,9 @@ def GetMissionXML():
                     </RewardForTouchingBlockType>
                     <AgentQuitFromTouchingBlockType>
                         <Block type="redstone_block"/>
+                        <Block type="obsidian"/>
+                        <Block type="redstone_ore"/>
+                        <Block type="redstone_ore"/>
                     </AgentQuitFromTouchingBlockType>
                     <ObservationFromGrid>
                       <Grid name="floorAll">
@@ -77,6 +80,7 @@ def GetMissionXML():
                         <max x="10" y="-1" z="10"/>
                       </Grid>
                   </ObservationFromGrid>
+                  <ObservationFromFullStats/>
                 </AgentHandlers>
               </AgentSection>
             </Mission>'''
@@ -85,9 +89,9 @@ def GetMissionXML():
 def build_model(lr=0.001):
     global ACTIONS
     model = Sequential()
-    model.add(Dense((MAP_LENGTH, MAP_WIDTH), input_shape=((MAP_LENGTH, MAP_WIDTH),)))
+    model.add(Dense(MAP_LENGTH * MAP_WIDTH, input_shape=(MAP_LENGTH * MAP_WIDTH, )))
     model.add(PReLU())
-    model.add(Dense((MAP_LENGTH, MAP_WIDTH)))
+    model.add(Dense(MAP_LENGTH * MAP_WIDTH))
     model.add(PReLU())
     model.add(Dense(len(ACTIONS)))
     model.compile(optimizer='adam', loss='mse')
@@ -114,7 +118,7 @@ class iKun(object):
     def train(self, batch_size=5):
         global ACTIONS
 
-        env_size = self.memory[0][0].shape[1]
+        env_size = MAP_LENGTH * MAP_WIDTH
         mem_size = len(self.memory)
         data_size = min(mem_size, batch_size)
 
@@ -125,14 +129,15 @@ class iKun(object):
             canvas, action, reward, canvas_next, game_over = self.memory[j]
             inputs[i] = canvas
             targets[i] = self.predict(canvas)
+            print("targets[i]:", targets[i])
             Q_sa = np.max(self.predict(canvas_next))
             if game_over:
                 targets[i][action] = reward
             else:
                 targets[i][action] = reward + self.gamma * Q_sa
 
-        self.model.fit(inputs, targets, epochs=8, batch_size=10)
-        print("loss:", self.model.evaluate(inputs, targets))
+        self.model.fit(inputs, targets, epochs=2, batch_size=5, verbose=0)
+        # print("loss:", self.model.evaluate(inputs, targets))
 
 
 class Maze(object):
@@ -146,36 +151,47 @@ class Maze(object):
 
     def get_position_target(self):
         global MAP_LENGTH, MAP_WIDTH
-        world_state = self.agent_host.getWorldState()
-        if world_state.number_of_observations_since_last_state <= 0:
-            return -1
+        grid = -1
+        world_state = agent_host.getWorldState()
+        while world_state.is_mission_running:
+            time.sleep(0.1)
+            world_state = agent_host.getWorldState()
+            if len(world_state.errors) > 0:
+                raise AssertionError('Could not load grid.')
 
-        msg = world_state.observations[-1].text
-        ob = json.loads(msg)
-        grid = ob.get(u'floorAll', 0)  # 1D list
+            if world_state.number_of_observations_since_last_state > 0:
+                msg = world_state.observations[-1].text
+                observations = json.loads(msg)
+                grid = observations.get(u'floorAll', 0)
+                break
 
         position = [-1, -1]
         target = [-1, -1]
-        for i in range(len(grid)):
-            if grid[i] == "redstone_block":
-                target = [i // MAP_WIDTH, i % MAP_WIDTH]
-            elif grid[i] == "emerald_block":
-                position = [i // MAP_WIDTH, i % MAP_WIDTH]
 
-        return position, target
+        if not grid == -1:
+            for i in range(len(grid)):
+                if grid[i] == "redstone_block":
+                    target = [i // MAP_WIDTH, i % MAP_WIDTH]
+                elif grid[i] == "emerald_block":
+                    position = [i // MAP_WIDTH, i % MAP_WIDTH]
+
+        return [position, target]
 
     def get_canvas(self):
         global SELF, TARGET
         canvas = np.copy(self.maze)
         canvas[self.position[0]][self.position[1]] = SELF
         canvas[self.target[0]][self.target[1]] = TARGET
-        return canvas
+        return canvas.reshape((1, -1))
 
     def run(self):
         global ACTIONS, LAND
         canvas = self.get_canvas()
 
         while True:
+            self.position, self.target = self.get_position_target()
+            print("position:", self.position)
+
             prev_canvas = canvas
             reward = 0
             rnd = random.random()
@@ -185,52 +201,74 @@ class Maze(object):
             else:
                 action = np.argmax(self.agent.predict(canvas))
 
+            print("action:", action)
             agent_host.sendCommand(ACTIONS[action])
+            time.sleep(2)
             world_state = self.agent_host.getWorldState()
             game_over = False if world_state.is_mission_running else True
 
             # Update recognized maze
-            if world_state.number_of_observations_since_last_state > 0:
-                msg = world_state.observations[-1].text
-                ob = json.loads(msg)
-                print(ob)
-                if "Reward" in ob:
-                    current_reward = ob["Reward"]
-                    if current_reward > self.reward:  # 100 for reaching target
-                        self.maze[self.position[0]][self.position[1]] = TARGET
-                    elif current_reward + 1 == self.reward:  # -1 for each step
-                        self.maze[self.position[0]][self.position[1]] = LAND
-                    elif game_over:  # -20 for falling
-                        self.maze[self.position[0]][self.position[1]] = AIR
-                        current_reward = self.reward - 20
-                    reward = current_reward - self.reward
-                    self.reward = current_reward
+            current_reward = 0
+            if len(world_state.rewards) > 0:
+                current_reward = world_state.rewards[-1].getValue()
+                if current_reward == 100:  # 100 for reaching target
+                    self.maze[self.position[0]][self.position[1]] = TARGET
+                elif current_reward == -1:  # -1 for each step
+                    self.maze[self.position[0]][self.position[1]] = LAND
 
-            status = [prev_canvas, action, reward, canvas, game_over]
+            if game_over:  # -20 for falling
+                self.maze[self.position[0]][self.position[1]] = AIR
+                current_reward = -20
+
+            print("current_reward:", current_reward)
+            self.reward += current_reward
+
+            status = [prev_canvas, action, current_reward, canvas, game_over]
+            print("status_reward:", status[2])
             self.agent.memorize(status)
             self.agent.train()
 
+            if game_over:
+                print("game over")
+                return 0
+
 
 if __name__ == '__main__':
-    my_client_pool = MalmoPython.ClientPool()
-    my_client_pool.add(MalmoPython.ClientInfo("127.0.0.1", 10000))
     mission_xml = GetMissionXML()
     agent_host = MalmoPython.AgentHost()
     agent_host.setDebugOutput(False)
 
     model = build_model()
     ikun = iKun(model)
+    maze = Maze(agent_host, ikun)
 
-    num_reps = 10
+    num_reps = 100000
     for iRepeat in range(num_reps):
         mission = MalmoPython.MissionSpec(mission_xml, True)
         mission_record = MalmoPython.MissionRecordSpec()
-        agent_host.startMission(mission, my_client_pool, mission_record, 0, "iKun")
+        my_client_pool = MalmoPython.ClientPool()
+        my_client_pool.add(MalmoPython.ClientInfo("127.0.0.1", 10000))
+
+        max_retries = 3
+        for retry in range(max_retries):
+            try:
+                agent_host.startMission(mission, my_client_pool, mission_record, 0, "iKun")
+                break
+            except RuntimeError as e:
+                if retry == max_retries - 1:
+                    print("Error starting mission", (iRepeat + 1), ":", e)
+                    exit(1)
+                else:
+                    time.sleep(2)
 
         world_state = agent_host.getWorldState()
         while not world_state.has_mission_begun:
             time.sleep(0.1)
-            maze = Maze(agent_host, ikun)
-            maze.run()
+            world_state = agent_host.getWorldState()
+            for error in world_state.errors:
+                print("Error:", error.text)
+
+        print("maze run")
+        maze.run()
 
     time.sleep(10000)
